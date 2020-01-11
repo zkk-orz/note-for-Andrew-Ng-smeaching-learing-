ae8198091f6d4fc1c6d2d23aefdf8c07c1af1ff5
zhangkaikai <zhangkaikai@inner.sss.com>
2020-01-11 15:08:17 +0800

    ex8

---
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -0,0 +1,61 @@
+function [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, ...
+                                  num_features, lambda)
+%COFICOSTFUNC Collaborative filtering cost function
+%   [J, grad] = COFICOSTFUNC(params, Y, R, num_users, num_movies, ...
+%   num_features, lambda) returns the cost and gradient for the
+%   collaborative filtering problem.
+%
+
+% Unfold the U and W matrices from params
+X = reshape(params(1:num_movies*num_features), num_movies, num_features);
+Theta = reshape(params(num_movies*num_features+1:end), ...
+                num_users, num_features);
+
+            
+% You need to return the following values correctly
+J = 0;
+X_grad = zeros(size(X));
+Theta_grad = zeros(size(Theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost function and gradient for collaborative
+%               filtering. Concretely, you should first implement the cost
+%               function (without regularization) and make sure it is
+%               matches our costs. After that, you should implement the 
+%               gradient and use the checkCostFunction routine to check
+%               that the gradient is correct. Finally, you should implement
+%               regularization.
+%
+% Notes: X - num_movies  x num_features matrix of movie features
+%        Theta - num_users  x num_features matrix of user features
+%        Y - num_movies x num_users matrix of user ratings of movies
+%        R - num_movies x num_users matrix, where R(i, j) = 1 if the 
+%            i-th movie was rated by the j-th user
+%
+% You should set the following variables correctly:
+%
+%        X_grad - num_movies x num_features matrix, containing the 
+%                 partial derivatives w.r.t. to each element of X
+%        Theta_grad - num_users x num_features matrix, containing the 
+%                     partial derivatives w.r.t. to each element of Theta
+%
+
+
+
+predicted_rates = X * Theta';
+error_rates = predicted_rates.* R - Y;
+cost_regularization_theta = sum(sum(Theta .^ 2)) * lambda /2;
+cost_regularization_X = sum(sum(X .^ 2)) * lambda /2;
+J = sum(sum(error_rates .^ 2)) / 2 + cost_regularization_theta + cost_regularization_X;
+gradient_regularization_X = lambda .* X;
+X_grad = error_rates * Theta + gradient_regularization_X;
+gradient_regularization_Theta = lambda .*  Theta;
+Theta_grad = error_rates' * X + gradient_regularization_Theta;
+
+
+
+% =============================================================
+
+grad = [X_grad(:); Theta_grad(:)];
+
+end
